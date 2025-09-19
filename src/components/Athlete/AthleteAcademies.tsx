5d1a47e92fcdb33dbfa92dfb11bdd3162102cc3d
dumbsumit <1321sumitsvjc@gmail.com>
2025-09-19 22:16:04 +0530

    removed contact botton on acdemis of athelete

--- src/components/Athlete/AthleteAcademies.tsx
+++ src/components/Athlete/AthleteAcademies.tsx
@@ -160,9 +160,7 @@ const AthleteAcademies: React.FC = () => {
 
                 {/* Action Button */}
                 <div className="mt-4">
-                  <button className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors font-medium">
-                    Contact Academy
-                  </button>
+                  
                 </div>
               </div>
             </div>
